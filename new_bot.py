ab0bfa271085c09ebfb807a7a5e9f3d92a137273
SarMurlich <fedotov@gfca.ru>
2026-01-19 19:27:25 +0300

    Update new_bot.py

--- new_bot.py
+++ new_bot.py
@@ -31,7 +31,7 @@ from json_storage import add_user_if_not_exists, add_tickets_for_payment
 Configuration.account_id = "1085561"
 Configuration.secret_key = "live_L2jrGwfcPBjEmTk_tJlzN7PaD36dPljqctXPrw0TVbU"
 TOKEN = my_key
-TICKET_PRICE = 1
+TICKET_PRICE = 3000
 
 # --- ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ ---
 storage = MemoryStorage()
